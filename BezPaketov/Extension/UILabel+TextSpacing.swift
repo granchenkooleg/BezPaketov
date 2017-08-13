0c006238dedebd535004464f5122382a12d3fb11
Oleg <granchenkooleg@gmail.com>
2017-08-13 23:23:22 +0300

    add extention UILabel + TextSpacing

---
+++ BezPaketov/Extension/UILabel+TextSpacing.swift
@@ -0,0 +1,20 @@
+//
+//  UILabel+TextSpacing.swift
+//  BezPaketov
+//
+//  Created by Oleg on 8/13/17.
+//  Copyright © 2017 Yura Granchenko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - UILabel (Text Spacing)
+
+extension UILabel {
+    func addTextSpacing(_ spacing: CGFloat){
+        let attributedString = NSMutableAttributedString(string: text!)
+        attributedString.addAttribute(NSKernAttributeName, value: spacing, range: NSRange(location: 0, length: text!.characters.count))
+        attributedText = attributedString
+    }
+}
